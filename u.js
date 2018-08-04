2e08b816d6896ff266979656662e715dd0d2c5e8
Miguel de Luis Espinosa <luis.miguelde@gmail.com>
2018-08-04 16:54:35 +0100

    remove un-needed files

--- u.js
+++
@@ -1,9 +0,0 @@
-function merge(content, values) {
-  console.log(values);
-  a = content.replace("\%first_name\%", values["first_name"]);
-  console.log(a);
-  return content;
-}
-
-
-module.exports.merge = merge;
